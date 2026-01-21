8929ee6edcef50c7b1f6406f44082a6401a1eb4e
Archisha Srivastava <archisha22link@gmail.com>
2026-01-21 11:41:49 +0530

    Add configuration constants

---
+++ constants.py
@@ -0,0 +1,31 @@
+"""
+Configuration constants for the Sound Sorting Visualizer
+"""
+
+# Window dimensions
+WIDTH = 1200
+HEIGHT = 700
+
+# Colors (R, G, B)
+BACKGROUND = (15, 15, 25)
+BAR_COLOR = (100, 200, 255)
+COMPARING_COLOR = (255, 100, 100)
+SWAPPING_COLOR = (255, 255, 100)
+SORTED_COLOR = (100, 255, 100)
+TEXT_COLOR = (255, 255, 255)
+HEADER_COLOR = (200, 200, 200)
+
+# Array settings
+ARRAY_SIZE = 100
+MIN_VALUE = 10
+MAX_VALUE = 500
+
+# Audio settings
+SAMPLE_RATE = 44100
+MIN_FREQUENCY = 200
+MAX_FREQUENCY = 800
+SOUND_DURATION = 0.05
+
+# Visualization settings
+FPS = 60
+BAR_SPACING = 2
